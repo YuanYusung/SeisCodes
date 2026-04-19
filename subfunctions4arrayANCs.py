59b3845f017955612a2189b5d37b94252a776b40
Yusong Yuan <20221002038@cug.edu.cn>
2026-04-19 10:55:16 +0800

    Update subfunctions4arrayANCs.py

--- subfunctions4arrayANCs.py
+++ subfunctions4arrayANCs.py
@@ -49,9 +49,88 @@ def dat2pickle():
 
     with open(fout,'wb') as handle:
         pickle.dump(st,handle,protocol=pickle.HIGHEST_PROTOCOL)
-
-    print(len(st))
     return st
 
-if __name__ == "__main__":
-    dat2pickle()
+def tapering(st,vmin,vmax):
+    '''
+    subroutine for tapering utilizing obspy
+    
+    tr - obspy trace object
+    taxis - time axis of the input trace
+    vmin,vmax - the moving out velocities of the tapering window
+    '''
+    npts = st[0].stats.npts; t0 = st[0].stats.b; delta = st[0].stats.delta
+    taxis = np.linspace(t0,t0+npts*delta-delta,npts)
+    st_copy = st.copy()
+    for i in range(len(st)):
+        tr = st[i]
+        dist = tr.stats.dist
+        tmin = dist/vmax; tmax = dist/vmin
+        ind_taper = (taxis >= tmin) & (taxis <= tmax)
+    
+        tr.data += tr.data[::-1]
+        tr_copy = tr.copy()
+        tr_copy.data = tr.data[ind_taper]
+        tr_copy.taper(max_percentage=0.05)
+    
+        data = np.zeros_like(tr.data)
+        data[ind_taper] = tr_copy.data
+        tr_copy.data = data
+
+        st_copy[i] = tr_copy
+    
+    return st_copy
+
+def plot_st(st):
+    """
+    Plots the wavefield of the input Stream object.
+    """
+    if len(st) == 0:
+        print("Error: Waveform stream is empty.")
+        return
+
+    #st = tapering(st, vmin=1.0, vmax=6.0)
+
+    wfs = []; dist_lst = []
+    nt = len(st)
+    for tr in st:
+        dist = tr.stats.dist
+        dist_lst.append(dist)
+        wf = tr.data.copy()
+        wf /= np.max(np.abs(wf))
+        wfs.append(wf)
+        
+    dist_arr = np.array(dist_lst)
+
+    tr_ref = st[0]
+    t0 = tr_ref.stats.b
+    npts = tr_ref.stats.npts; delta = tr_ref.stats.delta
+    taxis = np.linspace(t0, t0 + npts * delta - delta, npts)
+    
+    ind = np.argsort(dist_arr)
+    dist_sorted = dist_arr[ind]
+    wfs_sorted = np.array(wfs)[ind, :]
+    
+
+    X, Y = np.meshgrid(taxis, dist_sorted)
+    wfs_plot = wfs_sorted
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 7))
+    example_idx = nt // 2 
+    img = ax.pcolormesh(X, Y, wfs_plot, cmap='coolwarm', rasterized=True, shading='auto', vmin=-0.8, vmax=0.8) 
+    
+    dist_exp = dist_sorted[example_idx]
+    ax.plot(taxis, wfs_plot[example_idx, :] / 10. + dist_exp, 'k-', lw=2.0, 
+            label='Example Trace')
+        
+    ax.tick_params(labelsize=14) 
+    ax.set_title('ANCs', fontsize=16)
+    ax.set_xlim(-5, 50)
+    ax.set_xlabel('Correlation time (s)', fontsize=16)
+    ax.set_ylabel('Interstation distance (km)', fontsize=16)
+
+    fig.tight_layout()
+    os.makedirs('Figures', exist_ok=True)
+    output_path = 'Figures/ANCs_ZZ.png'
+    fig.savefig(output_path, format='png', dpi=300)
+    plt.close(fig)
